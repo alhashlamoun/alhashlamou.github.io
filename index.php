e1243ba6071e2b2fae4be45435035753e4d8089f
alhashlamoun <ahmadalhashalamoun18@gmail.com>
2022-03-03 16:14:28 +0300

    Add files

---
+++ index.php
@@ -0,0 +1,477 @@
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" dir="rtl">
+<head>
+<title>Ahmad Alhashlamoun</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="style.css" type="text/css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://use.fontawesome.com/751166d62e.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" ></script>
+<!--Jquery-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!--Floating WhatsApp css-->
+<link rel="stylesheet" href="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/floating-wpp.min.css">
+<!--Floating WhatsApp javascript-->
+<script type="text/javascript" src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/floating-wpp.min.js"></script>
+
+<link rel="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<section id="nav-bar">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">الافق العميق للاستشارات</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#slider">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">about</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#services">ervices</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#team">فريق العمل</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#testmonials">عملائنا</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">دبلومات</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#footer">تواصلوا معنا</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+</section>
+<!-------Slider------->
+<div id="slider">
+<div id="headerSlider" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#headerSlider" data-slide-to="0"></li>
+    <li data-target="#headerSlider" data-slide-to="1"></li>
+    <li data-target="#headerSlider" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+  
+    <div class="carousel-item active">
+      <img src="img/q.png" class="d-block img-fluid" alt="...">
+      <div class="carousel-caption">
+     
+      <h2>.التسويق القائم على البيانات بدعم من الإبداع</h2>
+      <h4>.الاحتمالات  مفتوحة على مصراعيها</h4>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/q.png" class="d-block img-fluid" alt="...">
+      <div class="carousel-caption">
+      <h5>الاتحاد العربي للحلول التسويقية</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img/q.png" class="d-block img-fluid" alt="...">
+      <div class="carousel-caption">
+      <h5>الاتحاد العربي للحلول التسويقية</h5>
+      </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#headerSlider" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" ></span>
+    <span class="visually-hidden"></span>
+  </a>
+  <a class="carousel-control-next" href="#headerSlider" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" ></span>
+    <span class="visually-hidden"></span>
+  </a>
+</div>
+</div>
+<!-----about----->
+<section id="about">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+			<h2>معلومات عنا</h2>
+			<div class="about-content">
+			الافق للاستشارات والتدريب  هي من أفضل شركات التسويق الإلكتروني الرائدة  مهمتنا بناء وتعزيز الصورة الذهنية الخاصة بكم بمختلف الوسائل التسويقية على شبكات التواصل الإجتماعي والإنترنت كما نوفر حلول التسويق الرقمي الشاملة للشّركات والأفراد، ويتركز
+			 عملنا مع عملاؤنا في الشركات الصغيرة والكبيرة والمحلات التجارية و المؤسسات الخيرية لنساعدهم في الوصول إلى جمهورهم بما يحقق أهدافهم من خلال التسويق الإلكتروني، ونتخصص في تحليل احتياجات العملاء وتحديد الأدوات التسويقية المناسبة للعمل والإنتاج الفني والمرئي وإقامة الفعاليات .
+وقد تأسسّت الشركة منذ أكثر من خمسة سنوات، وبدأت كفريق تسويق إلكتروني ثم توسع فريقنا إلى إنتاج الأفلام والإعلانات وإقامة الفعاليات والحملات على الإنترنت في خطوة نسعى من خلالها في جعل عملائنا سعداء من خلال تزويدهم بالأفكار الإبداعية والتنفيذ الجيد والتقدم الملحوظ في أعمالهم.
+			</div>
+			<button type="button" class="btn btn-primary">عرض المزيد >></button>
+			</div>
+			<div class="col-md-6 skills-bar">
+			<h2>تقييمنا</h2>
+			<p>بناء المواقع</p>
+			<div class="progress">
+			<div class="progress-bar" style="width: 99.9%">99.9%</div>
+			</div>
+			<p>حملات اعلانية</p>
+			<div class="progress">
+			<div class="progress-bar" style="width: 99.9%">99.9%</div>
+			</div>
+			<p>انتاج اعلاني وفني</p>
+			<div class="progress">
+			<div class="progress-bar" style="width: 99.9%">99.9%</div>
+			</div>
+			<p>تصميم رقمي</p>
+			<div class="progress">
+			<div class="progress-bar" style="width: 99.9%">99.9%</div>
+			</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-------services-------->
+<section id="services">
+<div class="container">
+<h1>خدماتنا</h1>
+
+<div class="row services">
+	<div class="col-md-3 text-center">
+		<div class="icon">
+		<i class="fa fa-desktop"></i>
+		</div>
+		<h3>تطوير المواقع</h3>
+		<p>موقع الويب الخاص بك هو أهم نقطة اتصال لأنه المحور الرئيسي الذي يضفي الحيوية على الشخصية الرقمية لعلامتك التجارية. أنت تعلم بالفعل أنه امتداد لقيم شركتك وعلامتك التجارية ، وأنه يجب أن يكون لا يُنسى. من الرسوم المتحركة الصغيرة إلى الشكل والمظهر ، كل عنصر من عناصر موقع علامتك التجارية لديه القدرة على ترك انطباع دائم.
+		</p>
+	</div>
+	
+	<div class="col-md-3 text-center">
+		<div class="icon">
+		<i class="fa fa-tablet"></i>
+		</div>
+		<h3>تطوير التطبيقات</h3>
+		<p>موقع الويب الخاص بك هو أهم نقطة اتصال لأنه المحور الرئيسي الذي يضفي الحيوية على الشخصية الرقمية لعلامتك التجارية. أنت تعلم بالفعل أنه امتداد لقيم شركتك وعلامتك التجارية ، وأنه يجب أن يكون لا يُنسى. من الرسوم المتحركة الصغيرة إلى الشكل والمظهر ، كل عنصر من عناصر موقع علامتك التجارية لديه القدرة على ترك انطباع دائم.
+		</p>
+	</div>
+	
+	<div class="col-md-3 text-center">
+		<div class="icon">
+		<i class="fa fa-line-chart"></i>
+		</div>
+		<h3>تسويق الكتروني </h3>
+		<p>موقع الويب الخاص بك هو أهم نقطة اتصال لأنه المحور الرئيسي الذي يضفي الحيوية على الشخصية الرقمية لعلامتك التجارية. أنت تعلم بالفعل أنه امتداد لقيم شركتك وعلامتك التجارية ، وأنه يجب أن يكون لا يُنسى. من الرسوم المتحركة الصغيرة إلى الشكل والمظهر ، كل عنصر من عناصر موقع علامتك التجارية لديه القدرة على ترك انطباع دائم.
+		</p>
+	</div>
+	
+	<div class="col-md-3 text-center">
+		<div class="icon">
+		<i class="fa fa-paint-brush"></i>
+		</div>
+		<h3>التصميم الجرافيكي</h3>
+		<p>موقع الويب الخاص بك هو أهم نقطة اتصال لأنه المحور الرئيسي الذي يضفي الحيوية على الشخصية الرقمية لعلامتك التجارية. أنت تعلم بالفعل أنه امتداد لقيم شركتك وعلامتك التجارية ، وأنه يجب أن يكون لا يُنسى. من الرسوم المتحركة الصغيرة إلى الشكل والمظهر ، كل عنصر من عناصر موقع علامتك التجارية لديه القدرة على ترك انطباع دائم.
+		</p>
+	</div>
+</div>
+</div>
+</section>
+<!------------------------------diploma-------------------------------->
+<section>
+
+</section>
+<!-------Team Mempers--------->
+<section id="team">
+<div class="container">
+<h1>فريقنا</h1>
+<div class="row">
+<div class="col-md-3 profile-pic text-center">
+<div class="img-box">
+<img alt="" src="img/moh.jpg" class="img-responsive"><!-- صور الفريق -->
+<ul>
+<a href="https://www.facebook.com/moh.oth.hash"> <li><i class="fa fa-facebook"></i></li></a>
+<a href="#"> <li><i class="fa fa-twitter"></i></li></a>
+<a href="https://www.instagram.com/alhashlamoon/?fbclid=IwAR1huhSYUf54DZg_he_7LCiHQSe1R3a_NZSyYqHG5SEWRqIEYVYOE0MTOOk"> <li><i class="fa fa-instagram"></i></li></a>
+<a href="https://www.linkedin.com/in/mohammad-alhashlamoun-3927611a4?fbclid=IwAR2JFBrXQWNSZNATadSxF-gqwjtOnNp8qXQwygPLLsPsS6JkPlN1xQSf444"> <li><i class="fa fa-linkedin"></i></li></a>
+</ul>
+</div>
+<h2>Mohammad Othman</h2>
+<h2>Founder/CEO</h2>
+</div>
+</div>
+</div>
+</section>
+<!----------custemers----------->
+<section id="custemers">
+<div class="container">
+<h1>عملائنا</h1>
+<p class="text-center">. بشهادة +150 عميل قمنا بزيادة الارباح خلال مدة قصيرة</p>
+<div class="row">
+<div class="col-md-4 text-center">
+<div class="profil">
+<img class="user" src="cust-logo/angels.jpg">
+</div>
+</div>
+<div class="col-md-4 text-center">
+<div class="profil">
+<img class="user" src="cust-logo/g-gift.jpg">
+</div>
+</div>
+<div class="col-md-4 text-center">
+<div class="profil">
+<img class="user" src="cust-logo/mmw.jpg">
+</div>
+</div>
+<div class="col-md-4 text-center">
+<div class="profil">
+<img class="user" src="cust-logo/chsk.jpg">
+</div>
+</div>
+<div class="col-md-4 text-center">
+<div class="profil">
+<img class="user" src="cust-logo/how.jpg">
+</div>
+</div>
+<div class="col-md-4 text-center">
+<div class="profil">
+<img class="user" src="cust-logo/ms.jpg">
+</div>
+</div>
+
+</div>
+</div>
+</section>
+<!-------------photo-gallary------------->
+<!----------------التعديل بالاسفل هذا قسم الpromoسابقا------------->
+<!--------------قاعدة بيانات للصور اذ يجب ان يكون جزء من المعرض في هذا القسم--------------->
+<section id="prom">
+<div class="container">
+<h1>من اعمالنا</h1>
+<p class="text-center">. بشهادة +150 عميل قمنا بزيادة الارباح خلال مدة قصيرة</p>
+<div class="row">
+<div class="col-md-4 text-center">
+<div class="profil">
+<img class="user" src="cust-logo/angels.jpg">
+</div>
+</div>
+<div class="col-md-4 text-center">
+<div class="profil">
+<img class="user" src="cust-logo/g-gift.jpg">
+</div>
+</div>
+<div class="col-md-4 text-center">
+<div class="profil">
+<img class="user" src="cust-logo/mmw.jpg">
+</div>
+</div>
+<div class="col-md-4 text-center">
+<div class="profil">
+<img class="user" src="cust-logo/chsk.jpg">
+</div>
+</div>
+<div class="col-md-4 text-center">
+<div class="profil">
+<img class="user" src="cust-logo/how.jpg">
+</div>
+</div>
+<div class="col-md-4 text-center">
+<div class="profil">
+<img class="user" src="cust-logo/ms.jpg">
+</div>
+</div>
+
+</div>
+</div>
+</section>
+<!------------التعديل بالاعلى هذا قسم الpromoسابقا------------->
+<!-------price-------->
+<section id="price">
+<div class="container">
+<h1>رأيك باسعارنا؟</h1>
+	<div class="row">
+		<div class="col-md-3">
+			<div class="single-price">
+				<div class="price-head">
+				<h2>Bronze</h2>
+				<p>$60/<span>شهرياً</span></p>
+				</div>
+				<div class="price-content">
+				<ul>
+				<li><i class="fa fa-check-circle"></i>
+				انشاء الحسابات وإدارتها (في حال عدم وجودها)</li>
+				<li>إنتاج التصاميم 9 تصميم</li>
+				<li>الرد على المتابعين والعملاء (أثناء الدوام الرسمي)</li>
+				<li>كتابة المحتوى التسويقي</li>
+				<li>تشغيل حملات إعلانية</li>
+				<li>إضافة Location على Google Maps</li>
+				<li>التسويق على Facebook</li>
+				<li>إرسال تقارير شهرية</li>
+				<li>إعادة التواصل مع العملاء المحتملين</li>
+				<li>لا يتضمن الإنضمام لبرنامج المكافأت</li>
+				<li>لا يشمل التمويل للإعلانات</li>
+				</ul>
+				</div>
+				<div class="button-price">
+				<a>احصل على العرض</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-md-3">
+			<div class="single-price">
+				<div class="price-head">
+				<h2>Selver</h2>
+				<p>$150/<span>شهرياً</span></p>
+				</div>
+				<div class="price-content">
+				<ul>
+				<li><i class="fa fa-check-circle"></i>
+				انشاء الحسابات وإدارتها (في حال عدم وجودها)</li>
+				<li>إنتاج التصاميم 9 تصميم</li>
+				<li>الرد على المتابعين والعملاء (أثناء الدوام الرسمي)</li>
+				<li>كتابة المحتوى التسويقي</li>
+				<li>تشغيل حملات إعلانية</li>
+				<li>إضافة Location على Google Maps</li>
+				<li>التسويق على Facebook</li>
+				<li>إرسال تقارير شهرية</li>
+				<li>إعادة التواصل مع العملاء المحتملين</li>
+				<li>لا يتضمن الإنضمام لبرنامج المكافأت</li>
+				<li>لا يشمل التمويل للإعلانات</li>
+				</ul>
+				</div>
+				<div class="button-price">
+				<a>احصل على العرض</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-md-3">
+			<div class="single-price">
+				<div class="price-head">
+				<h2>Gold</h2>
+				<p>$250/<span>شهرياً</span></p>
+				</div>
+				<div class="price-content">
+				<ul>
+				<li><i class="fa fa-check-circle"></i>
+				انشاء الحسابات وإدارتها (في حال عدم وجودها)</li>
+				<li>إنتاج التصاميم 9 تصميم</li>
+				<li>الرد على المتابعين والعملاء (أثناء الدوام الرسمي)</li>
+				<li>كتابة المحتوى التسويقي</li>
+				<li>تشغيل حملات إعلانية</li>
+				<li>إضافة Location على Google Maps</li>
+				<li>التسويق على Facebook</li>
+				<li>إرسال تقارير شهرية</li>
+				<li>إعادة التواصل مع العملاء المحتملين</li>
+				<li>لا يتضمن الإنضمام لبرنامج المكافأت</li>
+				<li>لا يشمل التمويل للإعلانات</li>
+				</ul>
+				</div>
+				<div class="button-price">
+				<a>احصل على العرض</a>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-md-3">
+			<div class="single-price">
+				<div class="price-head">
+				<h2>Platinum</h2>
+				<p>$350/<span>شهرياً</span></p>
+				</div>
+				<div class="price-content">
+				<ul>
+				<li><i class="fa fa-check-circle"></i>
+				انشاء الحسابات وإدارتها (في حال عدم وجودها)</li>
+				<li>إنتاج التصاميم 9 تصميم</li>
+				<li>الرد على المتابعين والعملاء (أثناء الدوام الرسمي)</li>
+				<li>كتابة المحتوى التسويقي</li>
+				<li>تشغيل حملات إعلانية</li>
+				<li>إضافة Location على Google Maps</li>
+				<li>التسويق على Facebook</li>
+				<li>إرسال تقارير شهرية</li>
+				<li>إعادة التواصل مع العملاء المحتملين</li>
+				<li>لا يتضمن الإنضمام لبرنامج المكافأت</li>
+				<li>لا يشمل التمويل للإعلانات</li>
+				</ul>
+				</div>
+				<div class="button-price">
+				<a>احصل على العرض</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+<!-------contact-us-now-------->
+<section id="contact">
+<div class="container">
+<h1>راسلنا وتمتع بافضل العروض لتنجح في بناء مستقبل ناجح لشركتك</h1>
+<div class="row">
+<div class="col-md-6">
+<form action="" class="contact-form">
+<div class="form-group">
+<input type="text" class="form-control" placeholder="الاسم">
+</div>
+<div class="form-group">
+<input type="tel" class="form-control" placeholder="رقم الهاتف">
+</div>
+<div class="form-group">
+<input type="text" class="form-control" placeholder="البريد الالكتروني">
+</div>
+<div class="form-group">
+<textarea class="form-control" placeholder="الرسالة">
+</textarea>
+</div>
+<button type="submit" class="btn btn-primary">ارسال الرسالة</button>
+</form>
+</div>
+<div class="col-md-6 contact-info">
+<div class="follow"><b>العنوان : </b><i class="fa fa-map-marker"></i> شارع الجامعة ,عمان ,الاردن</div>
+<div class="follow"><b>هاتفنا : </b><i class="fa fa-phone"></i> 4873784937</div>
+<div class="follow"><b>ايميل : </b><i class="fa fa-envelope-o"></i> fhdieh@jfdhi,com</div>
+<div class="follow"><label><b>التواصل الاجتماعي : </b></label>
+<a href="#"><i class="fa fa-facebook"></i></a>
+<a href="#"><i class="fa fa-instagram"></i></a>
+<a href="#"><i class="fa fa-youtube-play"></i></a>
+<a href="#"><i class="fa fa-google-plus"></i></a>
+</div>
+</div>
+</div>
+</div>
+</section>
+<!------------footer-------------->
+<section id="footer">
+<div class="container text-center">
+<h1>نفتخر بخدمتكم</h1>
+<p>جميع الحقوق محفوظة 2020</p>
+</div>
+</section>
+<!---------footer-end-------->
+<script src="js/smooth-scroll.polyfills.min.js"></script>
+<script>
+	var scroll = new SmoothScroll('a[href*="#"]');
+</script>
+<div id="WAButton">
+<script type="text/javascript">
+$(function() {
+  $('#WAButton').floatingWhatsApp({
+    phone: '1231231231', //WhatsApp Business phone number International format-
+    //Get it with Toky at https://toky.co/en/features/whatsapp.
+    headerTitle: 'نسعد بتواصلكم معنا!', //Popup Title
+    popupMessage: 'مرحبا كيف يمكننا مساعدتك?', //Popup Message
+    showPopup: true, //Enables popup display
+    buttonImage: '<img src="https://rawcdn.githack.com/rafaelbotazini/floating-whatsapp/3d18b26d5c7d430a1ab0b664f8ca6b69014aed68/whatsapp.svg" />', //Button Image
+    //headerColor: 'crimson', //Custom header color
+    //backgroundColor: 'crimson', //Custom background button color
+    position: "right"    
+  });
+});
+</script>
+</div>
+</body>
+</html>
